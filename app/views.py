088f9690ac88eb49ee0c00b45dc2522f94c62b33
Deborah M Ingabire <ingabineza@gmail.com>
2020-11-22 01:11:17 +0200

    mofified index.html and views.pyfiles

--- app/views.py
+++ app/views.py
@@ -9,12 +9,11 @@ def index():
     '''
     view toot page
     '''
-    sources=get_sources('general')
-    business_sources = get_sources('business')
+    sources=get_sources('business')
     sports_sources=get_sources('sports')
 
     technology_sources=get_sources('technology')
 
     entertainment_sources=get_sources('entertainment')
     title="News Highlighter"
-    return render_template('index.html', title=title, sources=sources ,business_sources=business_sources,sports_sources=sports_sources,technology_sources=technology_sources,entertainment_sources=entertainment_sources)
+    return render_template('index.html', title=title, sources=sources ,sports_sources=sports_sources,technology_sources=technology_sources,entertainment_sources=entertainment_sources)
