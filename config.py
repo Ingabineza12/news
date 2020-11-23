1cb9b3c07c7962e937dd4d8200446c21a0772500
Deborah M Ingabire <ingabineza@gmail.com>
2020-11-23 22:18:53 +0200

    modified config.py and start.sh

--- config.py
+++ config.py
@@ -1,19 +1,34 @@
 import os
-class Config:
-    NEWS_SOURCES_BASE_URL='https://newsapi.org/v2/sources?apiKey=25af597429964787a9d58e12e9746cf3'
-    ARTICLES_BASE_URL='https://newsapi.org/v2/everything?q=bitcoin&apiKey=25af597429964787a9d58e12e9746cf3'
-    NEWS_API_KEY=os.environ.get('NEWS_API_KEY')
 
-    @staticmethod
-    def init_app(app):
-        pass
+class Config:
+    '''
+    General configuration for the parent class
+    '''
+    SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    ARTICLES_BASE_URL ='https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+    NEWS_API_KEY = '25af597429964787a9d58e12e9746cf3'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
 class ProdConfig(Config):
+    '''
+    Production configuration child class
+
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
     pass
 
 class DevConfig(Config):
-    DEBUG=True
+    '''
+    Development configuration child class
 
-config_options={
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+    DEBUG = True
+
+config_options = {
     'development':DevConfig,
     'production':ProdConfig
 }
