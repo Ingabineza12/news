b6fa83c51bd1e5ba2a6375ab4859332915572966
Deborah M Ingabire <ingabineza@gmail.com>
2020-11-23 15:42:21 +0200

    modified views.py file

--- app/main/views.py
+++ app/main/views.py
@@ -1,21 +1,28 @@
 from flask import render_template,request,redirect,url_for
-from app import app
-from .request import get_sources
+from . import main
+from ..requests import get_sources,get_articles
+from ..models import Sources
 
-# Views
-@app.route('/')
-
+@main.route('/')
 def index():
 
     '''
     view toot page
     '''
-
-    business_sources=get_sources('business')
+    sources=get_sources('business')
     sports_sources=get_sources('sports')
 
     technology_sources=get_sources('technology')
 
     entertainment_sources=get_sources('entertainment')
     title="News Highlighte"
-    return render_template('index.html', title=title, business=business_sources,sports=sports_sources,technology=technology_sources,entertainment=entertainment_sources)
+    return render_template('index.html', title=title, sources=sources ,sports_sources=sports_sources,technology_sources=technology_sources,entertainment_sources=entertainment_sources)
+
+@main.route('/sources/<id>')
+def articles(id):
+    '''
+    Views articles pages
+    '''
+    articles =get_articles(id)
+    title=f'NH | {id}'
+    return render_template('articles.html', title=title,articles=articles)
