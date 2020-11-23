ddd69bb24e782be0a4f4bc08882e50d7aabdf66b
Deborah M Ingabire <ingabineza@gmail.com>
2020-11-23 16:31:02 +0200

    modified article.html and run.py

--- run.py
+++ run.py
@@ -1,4 +1,16 @@
-from app import app
+from app import create_app
+from flask_script import Manager, Server
 
-if __name__ == '__main__':
-    app.run()
+app= create_app('development')
+
+manager= Manager(app)
+manager.add_command('server',Server)
+
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests =unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+if __name__=='__main__':
+    manager.run()
