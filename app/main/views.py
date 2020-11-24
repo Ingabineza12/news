b92e557fe2cda3296576b5554f8c7f5db7406697
Deborah M Ingabire <ingabineza@gmail.com>
2020-11-24 13:28:12 +0200

    deployment to heroku

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-from ..request import get_sources, get_articles
+from ..requests import get_sources, get_articles
 from ..models import Sources
 
 # Views
